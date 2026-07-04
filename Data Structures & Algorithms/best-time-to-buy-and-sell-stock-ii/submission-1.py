5b32d5487b3027ff3cc3ab249ccd97c20f7347ca
bhkaushik14 <bhkaushik14@gmail.com>
2026-07-03 22:54:09 -0700

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    '''def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        i = 0
+
+        while i < len(prices) - 1:
+            if prices[i] < prices[i + 1]:
+                index = self.increase(prices[i:], i)
+                print(index)
+                profit += prices[index] - prices[i]
+                i = index
+            i += 1
+
+        return profit
+
+    def increase(self, prices: List[int], currentIndex) -> int:
+        count = currentIndex
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i+1]:
+                count += 1
+            else:
+                break
+        
+        return count'''
+
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i - 1] < prices[i]:
+                profit += prices[i] - prices[i-1]
+        
+        return profit
+
+
+
