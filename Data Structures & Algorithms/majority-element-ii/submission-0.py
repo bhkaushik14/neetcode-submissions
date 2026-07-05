6937fd8cc043f7f50bdadaf0b82660bb371eb014
bhkaushik14 <bhkaushik14@gmail.com>
2026-07-04 22:22:49 -0700

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        print(nums)
+        n = len(nums)
+
+        count = {}
+        over3 = set()
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+            if count[num] > (n/3):
+                over3.add(num)
+        
+        return list(over3)
