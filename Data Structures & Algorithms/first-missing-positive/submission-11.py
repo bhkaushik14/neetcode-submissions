8f5bd515682dd369f409a89bf39248b7130cf8ff
bhkaushik14 <bhkaushik14@gmail.com>
2026-07-07 00:37:32 -0700

    Add: first-missing-positive - submission-11

---
+++ Data Structures & Algorithms/first-missing-positive/submission-11.py
@@ -0,0 +1,33 @@
+class Solution:
+    '''
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        
+        # Wrong Solution
+        # O(n) Time Complexity
+        # O(n) Auxiliary Space (Using Set)
+        
+        setNums = set(nums)
+
+        index = 1
+        while index <= len(nums) + 1:
+            if index not in setNums:
+                return index
+            index += 1
+    '''
+
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                correct_index = nums[i] - 1
+                nums[i], nums[correct_index] = nums[correct_index], nums[i]
+
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+
+        return n + 1
+        
+        
+            
