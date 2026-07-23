410497c132e4226f1c8b0403ef78ef04aa42f339
Bhavya Kaushik <299313310+bhkaushik14@users.noreply.github.com>
2026-07-22 18:43:00 -0700

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.py
@@ -0,0 +1,53 @@
+class Solution:
+    '''
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        hold = []
+
+        index = 0
+        while index < len(nums):
+            for i in range(index+1, len(nums)):
+                curr = nums[index] + nums[i]
+                for j in range(i + 1, len(nums)):
+                    curr_l = sorted([nums[index], nums[i], nums[j]])
+                    if curr + nums[j] == 0 and curr_l not in hold:
+                        hold.append(curr_l)
+            index += 1
+        return hold
+    '''
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
+        t3sum = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left, right = i+1, len(nums) - 1
+            num = nums[i]
+
+            if nums[i] > 0:
+                break
+
+            while left < right:
+                total = num + nums[left] + nums[right]
+                if total == 0:
+                    t3sum.append([num, nums[left], nums[right]])
+
+                    left_val, right_val = nums[left], nums[right]
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == left_val :
+                        left += 1
+
+                    while right > left and nums[right] == right_val:
+                        right -= 1 
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return t3sum
+
