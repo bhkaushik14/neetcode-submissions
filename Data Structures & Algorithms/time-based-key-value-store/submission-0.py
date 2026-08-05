2f4e1483e964df1f0d7077bc02186321f7447737
Bhavya Kaushik <299313310+bhkaushik14@users.noreply.github.com>
2026-08-04 18:51:37 -0700

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,23 @@
+class TimeMap:
+
+    def __init__(self):
+        self.dynamicdict = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.dynamicdict[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        entries = self.dynamicdict[key]
+        l, r = 0, len(entries) - 1
+        saved_val = ""
+        print(entries)
+        while l <= r:
+            mid = (l + r) // 2
+            if entries[mid][1] <= timestamp:
+                saved_val = entries[mid][0]
+                l = mid + 1
+            else:
+                r = mid - 1
+            
+        return saved_val
+        
